27c3e7f023af2accea7851a067de6dcf5aa0ee67
Ryan Tang Li Jian <87563782+RyTang@users.noreply.github.com>
2021-07-17 22:39:37 +0800

    Added FIT2004 Assignment 3 Questions
    Algorithms used to solve Assignment 3 Questions have been uploaded.

---
+++ Algorithm and Data Structures/DNA Fragments.py
@@ -0,0 +1,198 @@
+from typing import List
+from typing import Tuple
+
+"""
+___author___: "Ryan Li Jian Tang"
+___university___: "Monash University"
+
+Brief 1:
+The task of this code was to maintain a database that was used in a study for drug resistances. The database is used to store Bacterial DNA collected from patients. Each bacterial DNA contains a drug resistant gene sequence. Researchers will need to be able to add new sequences and query existing sequences in the database. 
+
+Queries return a string that satisfies the following conditions:
+    - Must contain the sequence queried as the prefix
+    - Have a higher frequency in the database than any other sequences with q as prefix
+    - Return lexicographically least if multiple strings are tied for query
+
+DNA sequence is typically denoted using A, T, C and G. However for ease of coding and proving that the functionalities work. A, B, C and D will be used instead due to adjacent ASCII Values.
+
+Methodology:
+As I need to be able to satisfy the requirements of the Query function. I would need to maintain a frequency count of each inserted genome string while effectively maintaining the structure. Thus, the best way to do that will be the use of a Trie data structure. Currently a trie is currently the fastest way that I know of to efficiently insert and query for a given string. Simply enough, insert a genome string and if it exists update the frequency at the terminal node. Then to have an efficient query, each node will store a reference to the highest frequency and least lexicographically string. Thus, if a prefix is given the query will only require to traverse len(prefix) nodes to obtain the answer. The references are updated after each insertion of a genome string. This is done by backtracking from the terminal node to the root node and updating the references if the inserted node now has a higher frequency than the current highest.
+
+"""
+
+
+class Node:
+    """
+    A node used to store data payloads and containing links to connected nodes
+    """
+
+    def __init__(self, frequency=0, size: int = 27) -> None:
+        """
+        Creates a node of certain size with certain characteristics.
+
+        :param frequency: Number of appearance for this terminal node
+        :param size: Number of links that should be created per node
+        :time_complexity: O(1)
+        """
+        # Terminal $ at index 0
+        self.links = [None] * size
+        self.previous = None
+        # data payload
+        self.frequency = frequency
+        self.highest_link = None
+        self.highest_index = size + 1
+        self.key = None
+
+
+class Trie:
+    """
+    A Trie specialised to store Characters using ASCII indexes
+    """
+
+    TRAVERSE_CREATE = 0
+    TRAVERSE_QUERY = 1
+
+    def __init__(self, size: int = 26, ascii_start_index: int = 97) -> None:
+        """
+        Creation of Trie with specifications from arguments.
+
+        :param size: The number of links a node should have.
+        :param ascii_start_index: The position of the smallest character used in the ASCII table.
+        :time_complexity: O(1)
+        """
+        self.start_index = ascii_start_index
+        self.node_size = size + 1
+        self.root = Node(size=self.node_size)
+        self.root.index = 0
+
+
+    def insert(self, key: str) -> None:
+        """
+        Traverses the path given by the key and updates the frequency of said key in terminal node.
+
+        :param key: Key that you would like to access
+        :time_complexity: O(n) where n is len(key).
+        """
+        current = self.root
+        # Go through key's path
+        current = self.__traverse(current, key)[0]
+
+        if current.links[0] is not None:  # Go through terminal
+            current = current.links[0]
+        else:  # Create Terminal
+            temp = current
+            current.links[0] = Node(frequency=0, size=0)  # Terminal node should have frequency
+            current = current.links[0]
+            current.key = key  # Store Key at terminal
+            current.previous = temp  # Keep track of previous node
+            current.index = 0
+            current.highest_index = None
+
+        # Update Frequency
+        current.frequency += 1
+
+        # Backtrack to root and update data
+        self.__backtrack(current.frequency, current, current.previous)
+        return
+
+
+    def __traverse(self, current: Node1, key: str, mode: int = TRAVERSE_CREATE) -> Tuple[Node1, int]:
+        """
+        Traverses the key's path in the trie, while performing operations depending on "mode".
+
+        :param current: The node from which you would like to begin from.
+        :param key: The path you would like to travel through in the trie.
+        :param mode: The mode of operation you would like to do, use modes listed at top of class.
+        :return: A Tuple containing the current node after traversing and the number of traversals to this node.
+        :time_complexity: O(n) where n is len(key).
+        """
+        trie_level = 0  # Is used to keep track of the Level of the trie currently on
+        for char in key:
+            index = ord(char) - self.start_index + 1
+            if current.links[index] is not None:
+                trie_level += 1
+                current = current.links[index]
+            elif mode == self.TRAVERSE_CREATE:  # Generate node when there is no linking node in Create mode
+                temp = current
+                current.links[index] = Node1(size=self.node_size)
+                current = current.links[index]
+                current.previous = temp
+                current.index = index
+            elif mode == self.TRAVERSE_QUERY:  # Stop search as key does not exist in Searching Mode
+                break
+        return current, trie_level
+
+
+    def __backtrack(self, freq: int, link: Node1, start: Node1) -> None:
+        """
+        Backtracks from terminal to root, updates data along the way.
+
+        :param freq: Current frequency of terminal node.
+        :param link: Link to terminal node.
+        :param start: From which node would you like to backtrack from.
+        :time_complexity: O(n) where n is the number of nodes to traverse back to root.
+        """
+        current = start
+        last_index = 0    # As last node was a terminal Node
+
+        while current is not None:
+            state1 = current.frequency < freq
+            state2 = current.frequency == freq and current.highest_index >= last_index
+            if state1 or state2:    # If the previous node I was at has a better link than current
+                current.frequency = freq
+                current.highest_index = last_index
+                current.highest_link = link
+            link = current.highest_link
+            last_index = current.index
+            current = current.previous
+
+
+    def query(self, prefix: str) -> str:
+        """
+        Looks for a valid key with the highest frequency in lexicographical order.
+
+        :param prefix: The start of a valid key that you would like to query.
+        :return: Key with the highest frequency or None if there is none.
+        :time_complexity: O(n) where n is len(rev_prefix)
+        """
+        current = self.root
+        ret_str = None
+        current, counter = self.__traverse(current, prefix)
+        # If found a similar prefix
+        if counter == len(prefix) and current.highest_link is not None:
+            current = current.highest_link
+            ret_str = current.key
+        return ret_str
+
+
+class SequenceDatabase:
+    def __init__(self) -> None:
+        """
+        Instantiate a Trie data structure with fixed size.
+
+        :time_complexity: O(1)
+        """
+        self.trie = Trie(size=4, ascii_start_index=65)
+        return
+
+
+    def addSequence(self, s: str) -> None:
+        """
+        Traverses the path given by the key and updates the frequency of said key in terminal node.
+
+        :param s: Key that you would like to access
+        :time_complexity: O(n) where n is len(s).
+        """
+        self.trie.insert(key=s)
+        return
+
+
+    def query(self, q: str) -> str:
+        """
+        Looks for a valid key with the highest frequency in lexicographical order.
+
+        :param q: The start of a valid key that you would like to query.
+        :return: Key with the highest frequency or None if there is none.
+        :time_complexity: O(n) where n is len(q)
+        """
+        return self.trie.query(q)
---
+++ Algorithm and Data Structures/OrfFinder.py
@@ -0,0 +1,174 @@
+from typing import List
+from typing import Tuple
+
+"""
+___author___: "Ryan Li Jian Tang"
+___university___: "Monash University"
+
+Brief:
+In Molecular Genetics, there is a notion of Open Reading Frame (ORF). An ORF is a portion of DNA that is used as the blueprint for a protein. All ORFs start with a particular sequence and end with a particular sequence. 
+
+The task is to find all sections of a genome which start with a given sequence of characters and end with a possibly different given sequence of characters 
+
+DNA sequence is typically denoted using A, T, C and G. However for ease of coding and proving that the functionalities work. A, B, C and D will be used instead due to adjacent ASCII Values.
+
+Methodology:
+The idea is to maintain the genome in a trie data structure for easier searching. In particular a prefix trie of the genome is created, followed by a suffix trie of the reversed genome (An Example is given below to illustrate how this looks). In each node of the trie, there will be a record that contains parts of genomes that has the related prefix or suffixes of the remainder trie. Then what needs to be done is to conduct a search of the starting genome in the prefix trie, and a search of the ending genome in the reversed suffix trie to retrieve 2 records from the different tries. Afterwards a check is done to compare if there are any available combinations between the 2 records. 
+
+Example. If the genome given was "ABCD":
+Prefix Trie: ["ABCD", "BCD" , "CD", "D"]
+Reversed Suffix Trie: ["DCBA", "CBA", "BA", "A"]
+
+
+  
+
+"""
+
+
+class Node:
+    """ A Class used to store data in a trie, contains links to connecting nodes"""
+
+    def __init__(self, size: int = 27):
+        """
+        Creates a node of certain size with certain characteristics.
+
+        :param size: Number of links that should be created per node
+        :time_complexity: O(1)
+        """
+        # Terminal $ at index 0
+        self.links = [None] * size
+        # data payload
+        self.indexes = []
+        self.last_added = None
+
+
+class PrefixSuffixTrie:
+    """ A Class that creates a Prefix or Suffix Trie of the given word depending on the mode chosen."""
+
+    MODE_SUFFIX = 0
+    MODE_PREFIX_REVERSED = 1
+
+
+    def __init__(self, word: str, mode: int = MODE_PREFIX_REVERSED, size: int = 26, ascii_start_index: int = 97) -> None:
+        """
+        Creation of Trie with specifications from arguments.
+
+        :param word: The word that is to be used in the generation
+        :param mode: Either make a rev_prefix or suffix trie, Default = suffix.
+        :param size: The number of links a node should have.
+        :param ascii_start_index: The position of the smallest character used in the ASCII table.
+        :time_complexity: O(N^2) where N is len(word).
+        """
+        self.word = word
+        self.mode = mode
+        self.start_index = ascii_start_index
+        self.node_size = size + 1
+        self.root = Node(size=self.node_size)
+
+        # Create Trie based on mode
+        for i in range(len(self.word)):
+            self.__insert(i)
+
+
+    def __insert(self, length: int) -> None:
+        """
+        Inserts a word into the trie based on length of the word
+
+        :param length: How long the word should currently be
+        :time_complexity: O(n) where n is the len(word)
+        """
+        current = self.root
+        start = length
+
+        if self.mode == self.MODE_SUFFIX:
+            end = len(self.word)
+            step = 1
+        else:
+            end = 0 - 1
+            step = -1
+
+        # Traverse word
+        for char_pos in range(start, end, step):
+            char = self.word[char_pos]
+            index = ord(char) - self.start_index + 1
+            # if next node not available, create one
+            if current.links[index] is None:
+                current.links[index] = Node(size=self.node_size)
+            # move to next node
+            current = current.links[index]
+            # Update node data
+            if current.last_added is None or current.last_added != char_pos:
+                current.indexes += [char_pos]
+                current.last_added = char_pos
+        return
+
+
+    def search(self, key: str) -> List[int]:
+        """
+        Searches for a key given and returns the indexes that have that substring in the word.
+
+        :param key: word that is to be searched in the trie.
+        :return: The list of indexes that have the key in the trie.
+        :time_complexity: O(n) where n is len(key)
+        """
+        current = self.root
+
+        for char in key:
+            index = ord(char) - self.start_index + 1
+            if current.links[index] is None:
+                return None
+            else:
+                current = current.links[index]
+        return current.indexes
+
+
+class OrfFinder:
+    """ Used to find all possible strands in the genome given that begins and ends in a certain pattern efficiently"""
+
+    def __init__(self, genome: str) -> None:
+        """
+        Create a Suffix Trie of the reversed genome and a normal Prefix Trie of the genome.
+
+        :param genome: Genome that is to be searched on.
+        :time_complexity: O(n^2) where n is len(genome).
+        """
+        self.genome = genome
+        self.suffix = PrefixSuffixTrie(genome, mode=PrefixSuffixTrie.MODE_SUFFIX, size=4, ascii_start_index=65)
+        self.prefix_rev = PrefixSuffixTrie(genome, mode=PrefixSuffixTrie.MODE_PREFIX_REVERSED, size=4, ascii_start_index=65)
+        return
+
+
+    def find(self, start: str, end: str) -> List[str]:
+        """
+        Finds all valid choices that begins with "start" and ends with "end" within the genome.
+
+        :param start: Series of characters that the choice must begin with
+        :param end: Series of characters that the choice must end with
+        :return: All valid choices within given genome
+        :time_complexity: O(len(start) + len(end) + N) where N is the number of combinations of start and end.
+        """
+        ar = []
+        valid = True
+
+        # Search start in suffix trie and get the list of indexes
+        front = self.suffix.search(start)
+        # Search end reversed in prefix reversed trie and get the list of indexes
+        end_rev = end[::-1]
+        back = self.prefix_rev.search(end_rev)
+        if front is None or back is None:
+            valid = False
+
+        # Get possible strings with no overlap
+        if valid:
+            i, j = 0, len(back) - 1
+            while i < len(front):
+                if front[i] >= back[j] or j < 0:
+                    j = len(back) - 1
+                    i += 1
+                else:
+                    start_index = front[i] - len(start) + 1
+                    end_index = back[j] + len(end)
+                    ar += [self.genome[start_index:end_index]]
+                    j -= 1
+        return ar
+
